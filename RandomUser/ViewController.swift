3132498e774386d38fb82a46987d282fa2a6a182
Karen <Madoyan.kar@gmail.com>
2021-01-30 23:23:38 +0400

    Initial Commit

---
+++ RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by Karen Madoyan on 2021/1/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
